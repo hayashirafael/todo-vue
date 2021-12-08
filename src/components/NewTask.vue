8cbac07218803b70884d60027db3374f1cb2ff6f
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-12-08 15:05:03 -0300

    <style> estilizando input e botao do NewTask

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="new-task">
-     <input v-model="name" @keydown.enter="add" type="text">
-     <button @click="add">+</button>
+     <input v-model="name" @keydown.enter="add" type="text" class="form" placeholder="Digite uma tarefa">
+     <button @click="add" class="form">+</button>
   </div>
 </template>
 
@@ -22,5 +22,30 @@ export default {
 </script>
 
 <style>
+   .new-task {
+      margin: 35px;
+   }
 
+   .form {
+      outline: none;
+      font-size: 1.7rem;
+      border: 1px groove rgb(255, 255, 255);
+      padding: 5px 10px 8px;
+      color: #fff;
+   }
+
+   input.form {
+      width: 420px;
+      background: #fff2;
+      border-top-left-radius: 8px;
+      border-bottom-left-radius: 8px;
+
+   }
+
+   button.form {
+      border-left: none;
+      background-color: #2196f3;
+      border-top-right-radius: 8px;
+      border-bottom-right-radius: 8px;
+   }
 </style>
