f659f911cfc31a6f9af60b2d8e77eec2b4b10a8e
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-12-08 10:38:11 -0300

    <test>importando componente filho e passando props

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
     <h1>Tarefas</h1>
+    <TaskGrid :tasks="tasks"/>
   </div>
 </template>
 
 <script>
-
+import TaskGrid from './components/TaskGrid.vue'
 export default {
   name: 'App',
   components: {
+    TaskGrid
   },
-  
+  data() {
+    return {
+      tasks: [
+        {name: 'Lavar louça', pending: false},
+        {name: 'Comprar roupa', pending: true},
+      ]
+    }
+  },
 }
 </script>
 
