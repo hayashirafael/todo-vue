d5bd5b10a85c5021b6bc91ba55f95c9621df62d4
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-12-08 11:29:29 -0300

    <chore> colocando funcionalidade para aparecer somente se houver tarefa
    <style> estilizando divs geradas a partir das tarefas

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="task-grid">
-     <template>
+     <template v-if="tasks.length">
         <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
      </template>
+     <p v-else class="no-task">Nenhuma task encontrada.</p>
 
   </div>
 </template>
@@ -21,5 +22,22 @@ export default {
 <style>
 body {
    color: #fff;
+}
+
+.task-grid {
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: center;
+   align-items: center;
+   align-content: center;
+}
+
+.task-grid .task {
+   margin: 10px;
+}
+
+.no-task {
+   color: #aaa;
+   font-size: 1.7rem;
 }
 </style>
