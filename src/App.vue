5e2c272083803a17724472f125659f4b85da7b84
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-12-09 10:17:53 -0300

    <feat> Adicionando e importando método para a barra de progresso das tasks concluídas

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>Tarefas</h1>
+    <TaskProgress :progress="progress" />
     <NewTask @taskAdded="addTask($event)"/>
     <TaskGrid :tasks="tasks" 
     @taskDeleted="deleteTask" 
@@ -9,19 +10,26 @@
 </template>
 
 <script>
+import TaskProgress from './components/TaskProgress.vue'
 import TaskGrid from './components/TaskGrid.vue'
 import NewTask from './components/NewTask.vue'
 export default {
   name: 'App',
   components: {
     TaskGrid,
-    NewTask
+    NewTask,
+    TaskProgress
   },
   data() {
     return {
-      tasks: [
-        
-      ]
+      tasks: [],
+    }
+  },
+  computed: {
+    progress() {
+      const total = this.tasks.length
+      const done = this.tasks.filter(t => !t.pending).length
+      return Math.round(done / total * 100) || 0
     }
   },
   methods: {
---
+++ src/components/TaskProgress.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="task-progress">
+     <span class="progress">{{progress}}%</span>
+     <div class="bar-progress" :style="{width: progress + '%'}"></div>
+  </div>
+</template>
+
+<script>
+export default {
+   props: {progress: {type:Number, default: 0}}
+}
+</script>
+
+<style>
+   .task-progress {
+      position: relative;
+      height: 20px;
+      width: 80%;
+      border: 1px solid grey;
+      border-radius: 10px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+   }
+
+   .bar-progress {
+      position: absolute;
+      height: 100%;
+      background-color: #4CAF50;
+      border-radius: 10px;
+      align-self: flex-start;
+   }
+
+   .progress {
+      z-index: 1;
+      font-size: 1.2rem;
+   }
+
+</style>
